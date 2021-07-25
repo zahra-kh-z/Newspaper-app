11aa3c4dbfb0d1c34af1242032bc2809313c4db2
Zahra <zahra.kh2005@gmail.com>
2021-07-25 04:56:54 +0430

    chapter 8, 9, 10, 11, 13

---
+++ articles/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView  # new
+from django.urls import reverse_lazy  # new
+from .models import Article
+from django.views.generic.edit import UpdateView, DeleteView, CreateView  # new
+
+
+# Create your views here.
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+
+class ArticleDetailView(DetailView):  # new
+    model = Article
+    template_name = 'article_detail.html'
+
+
+class ArticleUpdateView(UpdateView):  # new
+    model = Article
+    fields = ('title', 'body',)
+    template_name = 'article_edit.html'
+
+
+class ArticleDeleteView(DeleteView):  # new
+    model = Article
+    template_name = 'article_delete.html'
+    success_url = reverse_lazy('article_list')
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'article_new.html'
+    fields = ('title', 'body', 'author',)
